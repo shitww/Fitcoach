de7480f390ba5abd5241a482532eecdf5f3b0a41
shitww <783002775@qq.com>
2026-04-19 20:16:01 +0800

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">Hello FitCoach!</h1>
+      <p className="mt-4 text-lg text-gray-400">项目创建成功！</p>
+    </div>
+  );
+}
